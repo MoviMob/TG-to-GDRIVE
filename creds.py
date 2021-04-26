4c6dda388bc031b6bb485e00c41017c876b0192d
MoviMob <54040477+MoviMob@users.noreply.github.com>
2021-04-27 02:06:21 +0600

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,6 +1,6 @@
 class Creds():
     # ENTER Your bot Token Here
-    TG_TOKEN = ""
+    TG_TOKEN = "1634641342:AAHjGdy1MCArUu0XawyjImD6zlru9v1t4KY"
     
     
     
@@ -8,10 +8,10 @@ class Creds():
     # Because of pydrive And pydrive v2 Api
     
     #Folder Id Of Teamdrive
-    TEAMDRIVE_FOLDER_ID = ""
+    TEAMDRIVE_FOLDER_ID = "tqkbRSPw-Crc0g1AnxOMwWWxVwf6QUxmj"
     
     # Id of Team drive 
-    TEAMDRIVE_ID = ""
+    TEAMDRIVE_ID = "0AEVj2p50uInlUk9PVA"
     
     
     
